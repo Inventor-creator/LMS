f5bc5af3fe29c054a970b9a78830858f8ffc6451
Inventor-creator <aryapat1234@gmail.com>
2025-08-21 18:45:20 +0530

    init

---
+++ src/test/java/LearningManagementSystem/LearningManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package LearningManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
